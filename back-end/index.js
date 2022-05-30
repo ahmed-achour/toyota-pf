c1e9a553bcf7d3d05e2453e86d8e8db417cc9572
ahmed <ahmedachour789@gmail.com>
2022-05-30 11:50:20 +0100

    init

---
+++ back-end/index.js
@@ -0,0 +1,22 @@
+// import libs
+const express = require("express");
+
+// import database connection
+
+// import controllers
+
+
+// creation d'un objet express .
+const app = express();
+const port = 3000;
+
+// autorisé les données de type JSON
+app.use(express.json())
+
+
+
+app.get('/', (req,res) =>{
+    res.send("hello world")
+})
+
+app.listen(port, ()=>{ console.log(`🟢 Server started on port ${port}`); })
